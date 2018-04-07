8d52f3ebb2d0cafee197567bb52e3d94c63f4069
Keisuke TADA <ksk.tada@gmail.com>
2018-04-07 09:02:05 +0900

    Add tcp_proxy via upload (incompl

---
+++ tcp_proxy.py
@@ -0,0 +1,15 @@
+# -*- codint: utf-8 -*-
+
+import sys
+import socket
+import threading
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  try:
+    server.bind((localhost_, local_port))
+  except:
+    print '[!!] Failed to listen on %s:%d' % (local_host, local_port)
+
