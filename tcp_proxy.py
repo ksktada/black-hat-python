a701b1e8054df57eefb1dc02c378ae1d2c03a96c
Keisuke TADA <ksk.tada@gmail.com>
2018-04-08 23:10:54 +0900

    Update tcp_proxy.py via upload

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -9,7 +9,13 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
   try:
-    server.bind((localhost_, local_port))
+    server.bind((local_host, local_port))
   except:
     print '[!!] Failed to listen on %s:%d' % (local_host, local_port)
+    print '[!!] Check for other listening sockets or correct permissions'
+    sys.exit(0)
+
+  print '[*] Listening on %s:%d' % (local_host, local_port)
+
+  server.listen(5)
 
