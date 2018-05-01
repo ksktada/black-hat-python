89f817f88ec30d8f86874088f646f587f64a5bd9
Keisuke TADA <ksk.tada@gmail.com>
2018-05-01 21:56:13 +0900

    update tcp_proxy via upload

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -58,3 +58,17 @@ def main():
   server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 
 main()
+
+def hexdump(src, length=16):
+  result = []
+  digits = 4 if isinstance(src, unicode) else 2
+
+  for i in xrange(0, len(src), length):
+    s = src[i:i+length]
+    hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+    text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+    result.append(b"%04X   %-*s   %s" % (i, length*(digits + 1), hexa, text))
+
+    print b'\n'.join(result)
+
+
